844d4a3f0df8592c47410b46a5a8fc16f4eb37d7
LiuXiaoZhuang <2046703959@qq.com>
2016-08-26 15:41:50 +0800

    初始化项目

---
+++ CoreData-Demo/CoreData-Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoreData-Demo
+//
+//  Created by 刘小壮 on 16/8/26.
+//  Copyright © 2016年 刘小壮. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CoreData-Demo/CoreData-Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreData-Demo
+//
+//  Created by 刘小壮 on 16/8/26.
+//  Copyright © 2016年 刘小壮. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
